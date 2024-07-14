fe5612a1f66d88d2b217bfe8d0c8f0c5ae8af116
bellawatsonf <bellawatsonf@gmail.com>
2024-07-15 00:05:07 +0700

    add logo tab

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,75 +1,15 @@
 /* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from "react";
-import {
-  AppstoreOutlined,
-  DownOutlined,
-  MailOutlined,
-  MenuOutlined,
-  SettingOutlined,
-  UpOutlined,
-} from "@ant-design/icons";
-import type { MenuProps } from "antd";
-import { Menu } from "antd";
+import { DownOutlined, MenuOutlined } from "@ant-design/icons";
+
 import logo from "../assets/logo.svg";
 import { useNavigate } from "react-router-dom";
 
-type MenuItem = Required<MenuProps>["items"][number];
-
-const items: MenuItem[] = [
-  {
-    label: "Navigation One",
-    key: "mail",
-    icon: <MailOutlined />,
-  },
-  {
-    label: "Navigation Two",
-    key: "app",
-    icon: <AppstoreOutlined />,
-    disabled: true,
-  },
-  {
-    label: "Navigation Three - Submenu",
-    key: "SubMenu",
-    icon: <SettingOutlined />,
-    children: [
-      {
-        type: "group",
-        label: "Item 1",
-        children: [
-          { label: "Option 1", key: "setting:1" },
-          { label: "Option 2", key: "setting:2" },
-        ],
-      },
-      {
-        type: "group",
-        label: "Item 2",
-        children: [
-          { label: "Option 3", key: "setting:3" },
-          { label: "Option 4", key: "setting:4" },
-        ],
-      },
-    ],
-  },
-  {
-    key: "alipay",
-    label: (
-      <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-        Navigation Four - Link
-      </a>
-    ),
-  },
-];
 export default function HeaderView() {
-  const [current, setCurrent] = useState("mail");
   let navigate = useNavigate();
   const [open, setopen] = useState("close");
   const [show, setShow] = useState(false);
-
-  const onClick: MenuProps["onClick"] = (e) => {
-    console.log("click ", e);
-    setCurrent(e.key);
-  };
 
   useEffect(() => {
     console.log(open, "open");
